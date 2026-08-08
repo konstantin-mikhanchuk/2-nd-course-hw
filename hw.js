d271cf8da4e0604a4ed3b41bce4397fcd675e2e2
Konstantin <kostya.mixanchuk@gmail.com>
2026-08-08 14:10:28 +0300

    5я домашка

--- hw.js
+++ hw.js
@@ -1,55 +1,85 @@
-{
-   let i = 0;
-   do {
-      console.log("Привет");
-      i++;
-   } while (i < 2);
-} 
+function compar(a, b) {
+   if (a>=b) {
+      return b;
+   } else {
+      return a;
+   }
+}
+console.log(compar (8, 4)); 
+console.log(compar (6, 6)); 
 
-{
-   let a = 1;
-   while (a <= 5) {
-      console.log(a); 
-      a++;
-   }
-} 
 
-{
-   let b = 7;
-   while (b >= 7 && b <= 22) {
-      console.log(b); 
-      b++;
+function definit(c) {
+   if (c % 2 === 0) {
+      return 'Число четное';
+   } else {
+      return 'Число нечетное';
    }
-} 
+}
+console.log(definit (8)); 
+console.log(definit (3)); 
 
-{
-   const obj = {
-       Коля : 200,
-       Вася : 300,
-       Петя : 400
-   }
-   for (let key in obj) {
-      console.log(`${key}: ${obj[key]}`);
-   }
-} 
 
-{
-   let n = 1000;
-   let num = 0;
-   while (n >= 50) {
-      n /= 2;
-      num++;      
-   }
-   console.log(n);
-   console.log(num);
-} 
+function squar(d) {
+   console.log (d * d);
+}
+squar (11);
+function squarRet(e) {
+   return e * e;
+}
+console.log(squarRet (10));
 
-{
-   let fridayNumber = 1;
-   while (fridayNumber <= 31) {
-        if (fridayNumber % 7 === 1) {
-        console.log(`Сегодня пятница, ${fridayNumber}-е число. Необходимо подготовить отчет.`);
-    }
-    fridayNumber++;
+
+function correctAge (age) {
+   if (age < 0) {
+      return 'Вы ввели неправильное значение';
+   } else if (age > 0 && age <= 12) {
+      return 'Привет, друг!';
+   } else {
+      return 'Добро пожаловать!';
    }
-} 
+}
+alert(correctAge(prompt("Сколько вам лет?")))
+
+
+function number (a, b) {
+   a = Number(a);
+   b = Number(b);
+   if (isNaN(a) || isNaN (b)) {
+      return 'Одно или оба значения не являются числом';
+   } else {
+      return a * b;
+   }
+}
+
+
+function cube() {
+   let n = prompt("уважаемый проверяющий, введите число") 
+   n = Number (n);
+   if (isNaN(n)) {
+      return 'Переданный параметр не является числом';
+   } else {
+      return `${n} в кубе равняется ${n ** 3}`   
+   }
+}
+console.log(cube());
+
+
+let circle1 = {
+   radius: 10,
+   getPerimeter() {
+      return 2 * 3.14 * this.radius;
+   },
+   getArea() {
+      return 3.14 * (this.radius ** 2);
+   }
+}
+let circle2 = {
+   radius: 15,
+   getPerimeter() {
+      return 2 * 3.14 * this.radius;
+   },
+   getArea() {
+      return 3.14 * (this.radius ** 2);
+   }
+}
